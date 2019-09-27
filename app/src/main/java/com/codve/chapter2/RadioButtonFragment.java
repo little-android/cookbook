6e84594dcf356c0eaf733248050d5570fc19bcd0
jiangyu <jiangyuwise@gmail.com>
2019-09-27 10:59:19 +0800

    使用复选框

--- app/src/main/java/com/codve/chapter2/RadioButtonFragment.java
+++ app/src/main/java/com/codve/chapter2/RadioButtonFragment.java
@@ -5,17 +5,37 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RadioButtonFragment extends Fragment {
 
     private RadioGroup mRadioGroup;
     private RadioButton mRedRadioButton;
+
+    private TextView mTextView;
+
+    // 多选框
+    private CheckBox mCheckBoxApple;
+    private CheckBox mCheckBoxBanana;
+    private CheckBox mCheckBoxOrange;
+    private List<String> fruits;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fruits = new ArrayList<>();
+    }
 
     @Nullable
     @Override
@@ -40,7 +60,53 @@ public class RadioButtonFragment extends Fragment {
         boolean status = mRedRadioButton.isChecked();
         Log.i("radio", String.valueOf(status));
 
+        mTextView = (TextView) view.findViewById(R.id.text_view_fruits);
 
+        mCheckBoxApple = (CheckBox) view.findViewById(R.id.checkbox_apple);
+        mCheckBoxBanana = (CheckBox) view.findViewById(R.id.checkbox_banana);
+        mCheckBoxOrange = (CheckBox) view.findViewById(R.id.checkbox_orange);
+
+        mCheckBoxApple.setOnCheckedChangeListener(((compoundButton, b) -> {
+            String tmp = getString(R.string.checkbox_apple);
+            if (b) {
+                fruits.add(tmp);
+            } else {
+                fruits.remove(tmp);
+            }
+            setTextView();
+        }));
+
+        mCheckBoxBanana.setOnCheckedChangeListener(((compoundButton, b) -> {
+            String tmp = getString(R.string.checkbox_banana);
+            if (b) {
+                fruits.add(tmp);
+            } else {
+                fruits.remove(tmp);
+            }
+            setTextView();
+        }));
+
+        mCheckBoxOrange.setOnCheckedChangeListener(((compoundButton, b) -> {
+            String tmp = getString(R.string.checkbox_orange);
+            if (b) {
+                fruits.add(tmp);
+            } else {
+                fruits.remove(tmp);
+            }
+            setTextView();
+        }));
         return view;
+    }
+
+    private void setTextView() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("");
+        String prefix = "";
+        for (String fruit : fruits) {
+            builder.append(prefix);
+            builder.append(fruit);
+            prefix = ", ";
+        }
+        mTextView.setText(builder.toString());
     }
 }
